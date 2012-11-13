c34352d237cb177ee0539de7b4ece796856edf7d
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-11-13 19:19:48 +0000

    Created ActivityEvent

---
+++ ActivityStreams/Events/ActivityEvent.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ActivityStreams\Events;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * ActivityEvent
+ *
+ * @author barnabywalters
+ */
+class ActivityEvent extends Event{
+
+    /**
+     * Object
+     * @var ActivityStreams\ActivityStreams\ObjectInterface $object
+     */
+    protected $object;
+    
+    /**
+     * Verb
+     */
+    public $verb;
+    
+    /**
+     * Actor
+     */
+    public $actor;
+
+    /**
+     * Constructor
+     * 
+     * @param ActivityStreams\ActivityStreams\ObjectInterface $object
+     */
+    public function __construct($verb, $object, $actor = null) {
+        $this->verb = $verb;
+        $this->object = $object;
+        $this->actor = $actor;
+    }
+
+    /**
+     * Get Object
+     * 
+     * @return ActivityStreams\ActivityStream\ObjectInterface
+     */
+    public function getObject() {
+        return $this->object;
+    }
+    
+    public function getVerb() {
+        return $this->verb;
+    }
+    
+    public function getActor() {
+        return $this->actor;
+    }
+}
+
+// EOF
